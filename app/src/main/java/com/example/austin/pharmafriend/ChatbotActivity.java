8586d93d8bd7f2c39c5cfcabd2d83b3ca2edd396
stinface98 <austin.atmaja@gmail.com>
2017-09-16 02:24:22 -0400

    Created wireframe of login screen with login button that switches to chatbot activity

---
+++ app/src/main/java/com/example/austin/pharmafriend/ChatbotActivity.java
@@ -0,0 +1,13 @@
+package com.example.austin.pharmafriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatbotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatbot);
+    }
+}
