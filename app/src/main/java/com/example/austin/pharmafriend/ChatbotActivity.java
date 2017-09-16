7486cffb95fcdb1db4ca50a1896e784d9ec92501
stinface98 <austin.atmaja@gmail.com>
2017-09-16 03:56:49 -0400

    Made dropdown menu (to choose screens) on chatbot activity

--- app/src/main/java/com/example/austin/pharmafriend/ChatbotActivity.java
+++ app/src/main/java/com/example/austin/pharmafriend/ChatbotActivity.java
@@ -1,13 +1,39 @@
 package com.example.austin.pharmafriend;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ChatbotActivity extends AppCompatActivity {
-
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatbot);
+        String[] sectionTitleArray = {"Chatbot", "Profile", "Current Medications"};
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sectionTitleArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
