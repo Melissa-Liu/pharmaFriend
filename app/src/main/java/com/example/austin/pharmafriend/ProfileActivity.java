88c6cc3e3a851c46f23cf19a10a17ad7230f0a47
stinface98 <austin.atmaja@gmail.com>
2017-09-16 04:40:12 -0400

    Finished wireframe of pharmafriend

--- app/src/main/java/com/example/austin/pharmafriend/ProfileActivity.java
+++ app/src/main/java/com/example/austin/pharmafriend/ProfileActivity.java
@@ -2,26 +2,20 @@ package com.example.austin.pharmafriend;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-public class ChatbotActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chatbot);
-
+        setContentView(R.layout.activity_profile);
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mDrawerToggle);
@@ -29,15 +23,14 @@ public class ChatbotActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-    @Override
     public boolean onOptionsItemSelected(MenuItem item){
         if(mDrawerToggle.onOptionsItemSelected(item)){
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
-    public void profileClick(View v){
-        Intent i = new Intent(this, ProfileActivity.class);
+    public void chatbotClick(View v){
+        Intent i = new Intent(this, ChatbotActivity.class);
         startActivity(i);
     }
     public void currentMedsClick(View v){
@@ -45,6 +38,3 @@ public class ChatbotActivity extends AppCompatActivity {
         startActivity(i);
     }
 }
-
-
-
